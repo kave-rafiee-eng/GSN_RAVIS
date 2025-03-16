f909b372792ee46c98b69efd01ea9308526f125d
kave-rafiee-eng <kave.rafiee.eng.com>
2025-03-16 13:57:30 +0330

    a

--- main/ADVANCE/MONITORING/SEGMENT/SEG_list_data.js
+++ main/ADVANCE/MONITORING/SEGMENT/SEG_list_data.js
@@ -18,13 +18,13 @@ list_data.splice(0, 1);
 
 var obj = new Object();
 
-for ( var i=1; i<13; i++ ){
-    obj.ar = 2; obj.ad = 5;  obj.bit=i-1; obj.not=0;
+for ( var i=11; i>=0; i-- ){
+    obj.ar = 2; obj.ad = 5;  obj.bit=i; obj.not=0;
     list_data.push(["PB Main","H"+i , JSON.stringify(obj),"unknown",0  ]);
 }
 
-for ( var i=1; i<13; i++ ){
-    obj.ar = 2; obj.ad = 6;  obj.bit=i-1; obj.not=0;
+for ( var i=11; i>=0; i-- ){
+    obj.ar = 2; obj.ad = 6;  obj.bit=i; obj.not=0;
     list_data.push(["PB Cabin","H"+i , JSON.stringify(obj),"unknown",0  ]);
 }
 
